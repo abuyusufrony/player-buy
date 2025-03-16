098cd48ab1359c5968fd38276e337219d0d4aba2
abuyusufrony <abuyusufrony@gmail.com>
2025-03-16 16:45:56 +0600

    fotter-end

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import shadow from '../../assets/bg-shadow.png'
+import logo from '../../assets/logo-footer.png'
 
 const Footer = () => {
     return (
@@ -20,6 +21,7 @@ const Footer = () => {
                             <button className="btn btn-neutral join-item ml-3.5 bg-amber-600 text-black">Suscribe</button>
                         </div>
                     </div>
+                    <div className='absolute ml-96 w-max h-max'><img className=' w-20' src={logo} alt="" /></div>
                 </div>
                 <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 ">
                     <nav>
@@ -41,6 +43,16 @@ const Footer = () => {
                         <a className="link link-hover">Terms of use</a>
                         <a className="link link-hover">Privacy policy</a>
                         <a className="link link-hover">Cookie policy</a>
+                        <div className="join ">
+                            <div className=''>
+                                <label className="input validator join-item ">
+                                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></g></svg>
+                                    <input type="email" placeholder="mail@site.com" required />
+                                </label>
+                                <div className="validator-hint hidden">Enter valid email address</div>
+                            </div>
+                            <button className="btn btn-neutral join-item  bg-amber-600 text-black">Suscribe</button>
+                        </div>
                     </nav>
                 </footer>
             </div>
